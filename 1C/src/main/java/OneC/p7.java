0b67efc5041abbb847a6d0eefbbf7dccf1a51e81
anush <anush@Abhishek>
2026-04-19 00:12:21 +0530

    Initial commit

---
+++ 1C/src/main/java/OneC/p7.java
@@ -0,0 +1,27 @@
+/*2. Write a Java program to iterate a linked list in reverse order (using
+objlist.descendingIterator())*/
+
+package OneC;
+import java.util.Iterator;
+import java.util.LinkedList;
+public class p7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<String>	ls = new LinkedList<>();
+		ls.add("Orange");
+		ls.add("green");
+		ls.add("pink");
+		ls.add("red");
+		ls.add("Blue");
+//		ListIterator<String> listIterator = ls.listIterator(ls.size());
+//        while (listIterator.hasPrevious()) {
+//            System.out.println(listIterator.previous());
+//        }
+		Iterator<String> iterator =  ls.descendingIterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+			}
+	}
+
+}
